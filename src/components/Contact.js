4461a5887c3253996b4295ed7b8a0438d4766698
PJ Frias <PeejFrias@Gmail.com>
2018-04-19 09:19:17 -0400

    replace banner background

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,8 +6,8 @@ const Contact = () => (
     <div id="contact">
       <div className="contact-text">
 
-        <h2>Let's build something together</h2>
-        <h2>I'd love to hear from you</h2>
+        <h2>Let's build something amazing</h2>
+        {/* <h2>I'd love to hear from you</h2> */}
         {/* <h2>Contact Me</h2> */}
         {/* <p className="lead">Let's build something together</p>
         <p className="lead">I'd love to hear from you</p> */}
