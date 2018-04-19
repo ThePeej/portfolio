b6a9841c6c3d9abced8c00584c65133aa086aef3
PJ Frias <PeejFrias@Gmail.com>
2018-04-19 14:48:04 -0400

    add temp profile pic

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -3,7 +3,7 @@ var plugins = [{
       options: {"plugins":[],"pathToConfigModule":"src/utils/typography.js"},
     },{
       plugin: require('/home/pj/Documents/Dev/Portfolio/portfolio/node_modules/gatsby-plugin-google-fonts/gatsby-ssr.js'),
-      options: {"plugins":[],"fonts":["librebaskerville","open sans","permanent marker","pacifico"]},
+      options: {"plugins":[],"fonts":["librebaskerville","open sans","permanent marker"]},
     },{
       plugin: require('/home/pj/Documents/Dev/Portfolio/portfolio/node_modules/gatsby-plugin-react-helmet/gatsby-ssr.js'),
       options: {"plugins":[]},
