3e5223147665b16ba458e669ee3db6a5d49d4fdd
PJ Frias <PeejFrias@Gmail.com>
2018-04-23 11:46:23 -0400

    fix mobile nav button not closing

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,47 +1,62 @@
-import React from "react";
+import React, { Component } from 'react';
 
-const Navigation = () => (
-  <nav id="nav-wrap">
-    <a className="mobile-btn" href="#nav-wrap" />
-    <ul id="nav" className="nav">
-      {/* <li className="current"> */}
-      <li>
-        <a className="smoothscroll" href="#home">
-          Home
+class Navigation extends Component {
+  state = {  }
+
+  handleClick() {
+
+    let menu = document.getElementById('nav')
+    if (menu.style.display === '' || menu.style.display === "none" ) {
+      menu.style.display = "block";
+    } else {
+      menu.style.display = "none";
+    }
+  }
+
+  render() {
+    return (
+      <nav id="nav-wrap">
+        <a className="mobile-btn" href="#nav-wrap" onClick={this.handleClick} />
+        <ul id="nav" className="nav">
+          <li>
+            <a className="smoothscroll" href="#home">
+              Home
         </a>
-      </li>
+          </li>
 
-      <li>
-        <a className="smoothscroll" href="#about">
-          About
+          <li>
+            <a className="smoothscroll" href="#about">
+              About
         </a>
-      </li>
+          </li>
 
-      <li>
-        <a className="smoothscroll" href="#skills">
-          Skills
+          <li>
+            <a className="smoothscroll" href="#skills">
+              Skills
         </a>
-      </li>
+          </li>
 
-      <li>
-        <a className="smoothscroll" href="#portfolio">
-          Work
+          <li>
+            <a className="smoothscroll" href="#portfolio">
+              Work
         </a>
-      </li>
+          </li>
 
-      {/* <li>
+          {/* <li>
         <a className="smoothscroll" href="#testimonials">
           Testimonials
         </a>
       </li> */}
 
-      <li>
-        <a className="smoothscroll" href="#contact">
-          Contact
+          <li>
+            <a className="smoothscroll" href="#contact">
+              Contact
         </a>
-      </li>
-    </ul>
-  </nav>
-);
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
 
 export default Navigation;
